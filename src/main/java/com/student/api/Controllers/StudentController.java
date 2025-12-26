270b05bdf5d46cadafdaa821dc5a5ca54f1cf895
Kiran-Ghule <ghulekiran1608@gmail.com>
2025-12-26 18:28:35 +0530

    Model Mapper get Added

---
+++ src/main/java/com/student/api/Controllers/StudentController.java
@@ -0,0 +1,13 @@
+package com.student.api.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/Student")
+public class StudentController {
+
+
+
+}
