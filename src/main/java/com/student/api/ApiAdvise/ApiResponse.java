d18efa2923ccb0d564beea21545f5358204a0530
Kiran-Ghule <ghulekiran1608@gmail.com>
2025-12-28 23:00:09 +0530

    User Defined Response Format is Set

--- src/main/java/com/student/api/ApiAdvise/ApiResponse.java
+++ src/main/java/com/student/api/ApiAdvise/ApiResponse.java
@@ -1,4 +1,33 @@
 package com.student.api.ApiAdvise;
 
-public class ApiResponse {
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ApiResponse<T> {
+
+    //@JsonFormat(pattern = "dd:MM:yyyy hh:mm:ss")
+    private LocalDateTime date;
+    private T data;
+    private ApiError err;
+
+    public ApiResponse()
+    {
+        this.date=LocalDateTime.now();
+    }
+
+    public ApiResponse(T data)
+    {
+        this();
+        this.data=data;
+    }
+
+    public ApiResponse(ApiError err)
+    {
+        this();
+        this.err=err;
+    }
+
 }
